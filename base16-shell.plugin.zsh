c9e3f960c63e65e8e6085400d3b788f5d7e4caad
dopplgangr <dopplgangr@gmail.com>
2019-02-26 00:52:02 -0500

    updated zsh hook

--- base16-shell.plugin.zsh
+++ base16-shell.plugin.zsh
@@ -1,2 +1,2 @@
 BASE16_SHELL=$(dirname ${(%):-%x})
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+[ -n "$PS1" ] && [ -s $BASE16_SHELL/cli.sh ] && source $BASE16_SHELL/cli.sh
