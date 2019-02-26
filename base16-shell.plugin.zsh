253814083b3af47ec1ed903968f149a3d29f82b3
dopplgangr <dopplgangr@gmail.com>
2019-02-26 03:04:42 -0500

    fixed plugin file

--- base16-shell.plugin.zsh
+++ base16-shell.plugin.zsh
@@ -1,3 +1,3 @@
 BASE16_SHELL=$(dirname ${(%):-%x})
 [ -n "$PS1" ] && [ -s $BASE16_SHELL/cli.sh ] && source $BASE16_SHELL/cli.sh
-[ -f ~/.base16_theme ] && base16 restoretheme
+[ -f ~/.base16_theme ] && base16 restore
